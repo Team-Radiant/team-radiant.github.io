8dc18699709f054f1cd046807a924c5df9d7c727
Shabbir Ezzy <76464869+ezzyshabbir05@users.noreply.github.com>
2025-02-26 14:28:34 +0530

    fixed 404

--- 404.js
+++ 404.js
@@ -1,4 +1,4 @@
 document.querySelector(".btn").addEventListener('click',()=>{
     console.log("clicked");
-    window.location.href = "https://team-radiant.github.io/#home";
+    window.location.href = "index.html";
 });
